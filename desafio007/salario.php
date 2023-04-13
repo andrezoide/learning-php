5c0e85bde7ddd5ba5bf755d22ae8d51d83c9d8ba
Andre Urbaneck <andrepedrosou@hotmail.com>
2023-04-13 17:18:01 -0300

    challenge 7 - started

--- desafio007/salario.php
+++ desafio007/salario.php
@@ -5,13 +5,27 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <title>desafio 07</title>
 </head>
 
 <body>
+  <?php
+  $salary = $_REQUEST["salary"];
+  ?>
   <header>
     <h1>informe seu salario</h1>
   </header>
+  <main>
+    <section>
+      <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
+        <label for="salary">Seu Salário
+          <input type="number" name="salary" value="<?= $dividendo ?>"/>
+        </label>
+        <input type="submit" value="Calcular" name="calcular" />
+      </form>
+    </section>
+  </main>
 </body>
 
 </html>
