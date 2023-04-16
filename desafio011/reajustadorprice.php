d5a0abe6f9d8793dec64dcc5c62da8f2569d45af
Andre Urbaneck <andrepedrosou@hotmail.com>
2023-04-16 15:21:59 -0300

    challenge 11 - ok

--- desafio011/reajustadorprice.php
+++ desafio011/reajustadorprice.php
@@ -1,13 +1,50 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>reajustador de preços</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>reajustador de preços</title>
 </head>
+
 <body>
-    
+  <?php
+  $percent = $_REQUEST["percent"] ?? 0;
+  $value = $_REQUEST["value"] ?? 0;
+  $trated = number_format($value, 2, ",", ".");
+
+  $tratedvalue = $value / 100;
+
+  $result = $tratedvalue * $percent;
+  $sumresult = $result + $value;
+  $sumresulttrated = number_format($sumresult, 2, ",", ".");
+
+  ?>
+  <main>
+    <h1>
+      Reajustador de Preços
+    </h1>
+    <section>
+      <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
+        <label for="value">
+          Qual será o percentual de reajuste ?
+        </label>
+        <input type="number" name="value" value="<? $value ?>">
+
+        <label for="percent">
+          Qual será o percentual de reajuste ?
+        </label>
+        <input type="range" value="0" min="0" max="100" name="percent" value="<? $percent ?>" oninput="this.nextElementSibling.value = this.value">
+        <output>0</output>%
+        <input type="submit" value="Calular">
+      </form>
+    </section>
+    <?php
+    echo "O produto que custava <strong>R$$trated </strong>, com <strong>$percent% de aumento <br/> vai passar a custar R$$sumresulttrated</strong>"
+    ?>
+  </main>
 </body>
+
 </html>
