1f16ebf902b750f2afff08348c108a8c529b0385
Andre Urbaneck <andrepedrosou@hotmail.com>
2023-04-25 17:47:02 -0300

    updt

--- desafio013/caixaeletronico.php
+++ desafio013/caixaeletronico.php
@@ -10,9 +10,6 @@
 </head>
 
 <body>
-  <?php
-  $value = $_REQUEST["value"];
-  ?>
   <header>
     <h1>
       Caixa eletronico
@@ -23,37 +20,46 @@
       <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
         <label for="value">
           Valor:
-          <input type="number" name="value" value="<?= $value ?>">
+          <input min="2" type="number" name="value" value="<?= $value ?>">
         </label>
         <input type="submit" value="Depositar">
       </form>
 
-      <h2>
-        Notas disponiveis
-      </h2>
-      <div>
-        <ul>
-          <li>
-            <img src="notas/2.PNG" alt="Nota de 2 reais">
-          </li>
-          <li>
-            <img src="notas/5.PNG" alt="Nota de 5 reais">
-          </li>
-          <li>
-            <img src="notas/10.PNG" alt="Nota de 10 reais">
-          </li>
-          <li>
-            <img src="notas/20.PNG" alt="Nota de 20 reais">
-          </li>
-          <li>
-            <img src="notas/50.PNG" alt="Nota de 50 reais">
-          </li>
-          <li>
-            <img src="notas/100.PNG" alt="Nota de 100 reais">
-          </li>
-        </ul>
-      </div>
     </section>
+    <div>
+      <?php
+      $requestvalue = $_REQUEST["value"];
+      $value = (int)number_format($requestvalue, 2, ",", ".");
+      $cash5 = 5;
+      $cash10 = 10;
+      $cash50 = 50;
+      $cash100 = 100;
+
+      echo "<h2> Quantidade de notas para R$$value </h2>";
+      ?>
+      <?php
+      echo ""
+      ?>
+      <ul>
+        <li>
+          <?php 
+          if ($value % 2 == 0 or $value /  == 1){
+            echo "<script>alert('valor invalido');</script>";
+          }
+          ?>
+          <img src="notas/5.PNG" alt="5reais">
+        </li>
+        <li>
+          <img src="notas/10.PNG" alt="10reais">
+        </li>
+        <li>
+          <img src="notas/50.PNG" alt="Nota de 50 reais">
+        </li>
+        <li>
+          <img src="notas/100.PNG" alt="Nota de 100 reais">
+        </li>
+      </ul>
+    </div>
   </main>
 </body>
 
